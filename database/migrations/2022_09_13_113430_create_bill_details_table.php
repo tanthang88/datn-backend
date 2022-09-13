68b5cbedab86faad424221cfa03af2ba0c10d40f
coi24122002 <anhqb2002@gmail.com>
2022-09-13 20:42:26 +0700

    up

---
+++ database/migrations/2022_09_13_113430_create_bill_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->integer('price');
+            $table->integer('amount');
+            $table->integer('into_price');
+            $table->integer('sale');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill_details');
+    }
+};
