9c7f7bd63a338fd7765fb11b0d7ae443c50adcd0
Phạm Tấn Thắng <67085017+tanthang88@users.noreply.github.com>
2022-12-26 14:40:38 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,12 +34,12 @@ return [
             'root' => storage_path('app'),
             'permissions' => [
                 'file' => [
-                    'public' => 0644,
-                    'private' => 0600,
+                    'public' => 0777,
+                    'private' => 0755,
                 ],
                 'dir' => [
-                    'public' => 0755,
-                    'private' => 0700,
+                    'public' => 0777,
+                    'private' => 0755,
                 ],
             ],
         ],
